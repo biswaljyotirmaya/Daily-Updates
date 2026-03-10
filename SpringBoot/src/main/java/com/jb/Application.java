3433c65768d3869829bd0c021da9ba301e7d6541
biswaljyotirmaya <jyotirmayabiswal03@gmail.com>
2026-03-10 18:43:30 +0530

    SpringBoot base

--- SpringBoot/src/main/java/com/jb/Application.java
+++ SpringBoot/src/main/java/com/jb/Application.java
@@ -3,11 +3,14 @@ package com.jb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 import outer.OuterClass;
+import config.Configue;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackageClasses = { Application.class, OuterClass.class })
+@Import(Configue.class)
 public class Application {
 
 	public static void main(String[] args) {
---
+++ SpringBoot/src/main/java/config/Configue.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configue {
+	public Configue() {
+		System.out.println("Configure---------------------------");
+	}
+}
